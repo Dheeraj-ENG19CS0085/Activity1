a1a6f94276ba687cd321a5c1737cd347ea7cf423
Dheeraj-ENG19CS0085 <76113323+Dheeraj-ENG19CS0085@users.noreply.github.com>
2021-03-18 15:31:44 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,24 +1,30 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
-int h, b, d;
+#include<math.h>
+int x1,x2,y , y2;
+
 void input()
 {
-    printf("Enter the values of h, b and d respectively: "); 
-    scanf("%d%d%d", &h, &b, &d);
+    printf("Enter the (x,y) coordinates of point 1: ");
+    scanf("%d%d",&x1,&y);
+    printf("Enter the (x,y) coordinates of point 2: ");
+    scanf("%d%d", &x2, &y2);
 }
-float volume(int h, int b, int d)
-{
-    float vol;
-    float x, y;
-    x = h*b*d; y = d/b;
-    vol = (x+y)*1/3;
-    return vol;
-}
+
 void disp()
 {
-    float v;
-    v = volume(h,b,d);
-    printf("The volume of the given troboloid with h: %d, b: %d and d: %d is %f cube units\n", h,b,d,v);
+    float distance;
+    distance = dist() ;
+    printf("The distance between (%d,%d) and (%d,%d) is %f units\n", x1,y,x2,y2,distance);
+}
+int dist()
+{
+
+    float d, num, denom;
+    num = pow((x1-x2),2);
+    denom = pow((y-y2),2);
+    d = sqrt(num+denom);
+    return d;
 }
 int main()
 {
